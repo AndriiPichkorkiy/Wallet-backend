b2dab89f99c442dd1c5cbecfdb1492e54c09663a
MaximIvE <ivanukmaxim@gmail.com>
2022-12-07 01:21:16 +0200

    swafgger: GET getAll

--- controllers/transaction/getStatistic.js
+++ controllers/transaction/getStatistic.js
@@ -2,22 +2,9 @@ const { Transaction } = require('../../models/transaction')
 
 
 const getStatistic= async (req, res) => {
-    console.log('getDate');
-    const data = await Transaction.aggregate([
-        {
-            $group: {
-                _id: {
-                    month: { $month: '$createdAt' },
-                    year: { $year: '$createdAt' }
-                },
-                count: { $sum: 1 }
-            }
-        },
-        { $sort: { count: -1, '_id.year': -1, '_id.month': -1 } }
-    ]);
-
+    console.log('Run controller getStatistic')
     
-    res.json(data);
+    res.json({ "message": "Hello World!" })
 }
 
 module.exports = getStatistic;
