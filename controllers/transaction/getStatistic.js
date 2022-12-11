8f2090aa472b13c9e31e804703cab3592aea5595
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-12-11 16:29:54 +0200

    fix getStatistic

--- controllers/transaction/getStatistic.js
+++ controllers/transaction/getStatistic.js
@@ -1,61 +1,68 @@
 const { RequestError } = require("../../helpers");
-const { Transaction } = require('../../models/transaction');
-const { User } = require('../../models/user');
-const { getDates, round } = require('../../helpers');
-const categories = require('../../data/categories');
-
+const { Transaction } = require("../../models/transaction");
+const { User } = require("../../models/user");
+const { getDates, round } = require("../../helpers");
+const categories = require("../../data/categories");
 
 const getStatistic = async (req, res) => {
-    const { _id } = req.user;
-    const { year, month } = req.query;
+  const { _id } = req.user;
+  const { year, month } = req.query;
 
-    const { startDate, endDate } = getDates(year, month);
+  const { startDate, endDate } = getDates(year, month);
 
-    const data = await Transaction.aggregate([
+  const data = await Transaction.aggregate([
     {
-        $match: {
-            owner: _id,
-            // type: false,
-            date: { $gte: startDate, $lt: endDate }
-        },
+      $match: {
+        owner: _id,
+        // type: false,
+        date: { $gte: startDate, $lt: endDate },
+      },
     },
     {
-        $group: {
-            _id: "$category.id",
-            quantity: { $sum: "$amount" },
-        },
+      $group: {
+        _id: "$category.id",
+        quantity: { $sum: "$amount" },
+      },
     },
     {
-        $project: {
-            _id: 0,
-            categoryId: "$_id",
-            quantity: "$quantity"
-        },
+      $project: {
+        _id: 0,
+        categoryId: "$_id",
+        quantity: "$quantity",
+      },
     },
-    ]).sort({categoryId: 1});
+  ]).sort({ categoryId: 1 });
 
-    const { balance: totalBalance } = await User.findById(_id);
-    if (totalBalance === undefined) throw RequestError(500, "Server error");
-    
-    let stats = [];
-    let expenses = 0;
-    let income  = 0;
-    
-    for (const obj of data) {
-        let { categoryId } = obj;
-        quantity = round(obj.quantity);
-        if (Number(categoryId) > 10500) { income = round(income + quantity) }
-        else { expenses = round(expenses + obj.quantity); stats.push({id: categoryId, name: categories.expenses[categoryId], quantity })}
+  const { balance: totalBalance } = await User.findById(_id);
+  if (totalBalance === undefined) throw RequestError(500, "Server error");
+
+  const stats = [];
+  let expenses = 0;
+  let income = 0;
+
+  for (const obj of data) {
+    const { categoryId } = obj;
+    const quantity = round(obj.quantity);
+    if (Number(categoryId) > 10500) {
+      income = round(income + quantity);
+    } else {
+      expenses = round(expenses + obj.quantity);
+      stats.push({
+        id: categoryId,
+        name: categories.expenses[categoryId],
+        quantity,
+      });
     }
+  }
 
-    res.json({
-        stats,
-        totalStats: {
-            expenses,
-            income,
-            totalBalance
-        }
-    })
-}
+  res.json({
+    stats,
+    totalStats: {
+      expenses,
+      income,
+      totalBalance,
+    },
+  });
+};
 
 module.exports = getStatistic;
