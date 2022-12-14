8cab3a0531e1919668f4ac45faf9a70efe54e79c
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-12-14 23:42:48 +0200

    get current - added avatar into response

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,6 +1,6 @@
 const getCurrent = async (req, res, next) => {
-  const { name, email } = req.user;
-  res.json({ name, email });
+  const { name, email, avatar } = req.user;
+  res.json({ name, email, avatar });
 };
 
 module.exports = getCurrent;
